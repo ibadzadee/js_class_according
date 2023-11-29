68b3555cfbfc85de8dd20b8708ca51c200f797be
ibadzadeee <sara.ibadzade@gmail.com>
2023-11-29 16:48:14 +0400

    js

---
+++ script2.js
@@ -0,0 +1,14 @@
+let accordion = document.querySelectorAll(".accordion");
+
+
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", ()=> {
+      let p = accordion[i].firstElementChild;
+      if (p.style.display === "block") {
+        p.style.display = "none";
+      } else {  
+        p.style.display = "block";
+      }
+    });
+  }
